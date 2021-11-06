67d9574ed7f8d9914aee531467cb59671d1572fd
Miechu <miechowskip@outlook.com>
2021-11-06 22:41:46 +0100

    Upload zad 5

---
+++ Zad5_obiektowosc/app/CalcCtrl.class.php
@@ -0,0 +1,112 @@
+<?php
+// KONTROLER strony kalkulatora
+require_once $conf->root_path.'/libs/Messages.class.php';
+require_once $conf->root_path.'/app/CalcForm.class.php';
+require_once $conf->root_path.'/app/CalcResult.class.php';
+
+
+// Smarty
+require_once $conf->root_path.'/libs/smarty/Smarty.class.php';
+
+
+class CalcCtrl
+{
+
+    private $msgs;   //wiadomości dla widoku
+    private $form;   //dane formularza (do obliczeń i dla widoku)
+    private $result; //inne dane dla widoku
+
+    /**
+     * Konstruktor - inicjalizacja właściwości
+     */
+    public function __construct()
+    {
+        //stworzenie potrzebnych obiektów
+        $this->msgs = new Messages();
+        $this->form = new CalcForm();
+        $this->result = new CalcResult();
+    }
+
+// 1. pobranie parametrów
+    public function getParams()
+    {
+        $this->form->kwota = isset($_REQUEST['kwota']) ? $_REQUEST['kwota'] : null;
+        $this->form->lata = isset($_REQUEST['lata']) ? $_REQUEST['lata'] : null;
+        $this->form->procent = isset($_REQUEST['procent']) ? $_REQUEST['procent'] : null;
+    }
+// 2. walidacja parametrów z przygotowaniem zmiennych dla widoku
+    public function validate()
+    {
+
+// sprawdzenie, czy parametry zostały przekazane
+        if (!(isset($this->form->kwota) && isset($this->form->lata) && isset($this->form->procent))) {
+            //sytuacja wystąpi kiedy np. kontroler zostanie wywołany bezpośrednio - nie z formularza
+            $this->msgs->addError('Błędne wywołanie aplikacji. Brak jednego z parametrów.');
+        }
+
+// sprawdzenie, czy potrzebne wartości zostały przekazane
+        if ($this->form->kwota == "") {
+            $this->msgs->addError('Nie podano kwoty kredytu');
+        }
+        if ($this->form->lata == "") {
+            $this->msgs->addError('Nie podano okresu kredytowania');
+        }
+        if ($this->form->procent == "") {
+            $this->msgs->addError('Nie podano oprocentowania');
+        }
+
+//nie ma sensu walidować dalej gdy brak parametrów
+        if (!$this->msgs->isError()) {
+
+            // sprawdzenie, czy $kwota, $lata, $procent są liczbami
+            if (!is_numeric($this->form->kwota)) {
+                $this->msgs->addError('Kwota nie jest liczbą');
+            }
+            if (!is_numeric($this->form->lata)) {
+                $this->msgs->addError('Okres kredytowania nie jest liczbą całkowitą');
+            }
+            if (!is_numeric($this->form->procent)) {
+                $this->msgs->addError('Oprocentowanie nie jest liczbą');
+            }
+
+            // Sprawdzenie czy $kwota i $lata nie są 0.
+            if ($this->form->kwota <= 0 || $this->form->lata <= 0) {
+                $this->msgs->addError('Kwota i okres kredytowania muszą być większe od 0');
+            }
+            return !$this->msgs->isError();
+        }
+    }
+// 3. wykonaj zadanie jeśli wszystko w porządku
+        public function process()
+    {
+        $this->getparams();
+
+        if ($this->validate()) {
+
+                //konwersja parametrów na int i double
+            $this->form->kwota = doubleval($this->form->kwota);
+            $this->form->lata = intval($this->form->lata);
+            $this->form->procent = doubleval($this->form->procent);
+            $this->msgs->addInfo('Parametry poprawne.');
+
+            $this->result->kwota_calkowita = ((($this->form->procent / 100) * $this->form->kwota) + $this->form->kwota);
+            $this->result->result = ((($this->form->procent / 100) * $this->form->kwota) + $this->form->kwota) / ($this->form->lata * 12);
+            $this->msgs->addInfo('Wykonano obliczenia.');
+        }
+        $this->generateView();
+    }
+// 4. Wywołanie widoku z przekazaniem zmiennych
+    public function generateView()
+    {
+        global $conf;
+        $smarty = new Smarty();
+        $smarty->setTemplateDir($conf->root_path.'/templates');
+        $smarty->setCompileDir($conf->root_path.'/templates_c');
+
+        $smarty->assign('conf',$conf);
+        $smarty->assign('msgs',$this->msgs);
+        $smarty->assign('form',$this->form);
+        $smarty->assign('res',$this->result);
+        $smarty->display('calc_view.tpl');
+    }
+}
---
+++ Zad5_obiektowosc/templates/calc_view.tpl
@@ -0,0 +1,82 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Kalkulator kredytowy - Piotr Miechowski</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="{$conf->app_url}/assets/css/main.css" />
+	</head>
+	<body class="landing is-preload">
+		<div id="page-wrapper">
+
+			<!-- Banner -->
+				<section id="banner">
+					<h2>Kalkulator kredytowy</h2>
+					<p>Piotr Miechowski</p>
+				</section>
+
+			<!-- Main -->
+				<section id="main" class="container">
+					<section class="box special features">
+						<div class="features-row">
+							<section>
+								<form action="{$conf->app_url}/app/calc.php" method="post">
+									<div class="row gtr-50 gtr-uniform">
+									<div class="col-8 col-12-mobilep">
+										<h4>Kwota kredytu (zł):</h4>
+										<input type="text" name="kwota" id="id_kwota" value="{$form->kwota}" placeholder="Podaj kwotę kredytu" /><br />
+										<h4>Okres kredytowania (lata):</h4>
+										<input type="text" name="lata" id="id_lata" value="{$form->lata}" placeholder="Podaj okres kredytowania" /><br />
+										<h4>Oprocentowanie (%):</h4>
+										<input type="text" name="procent" id="id_procent" value="{$form->procent}" placeholder="Podaj oprocentowanie" />
+									</div>
+									<div class="col-4 col-12-mobilep">
+										<br /><br /><br /><br /><br /><input type="submit" value="Oblicz" class="fit" />
+									</div>
+									</div>
+								</form>
+							</section>
+							<section>
+								<span class="icon solid major fa-chart-area accent3"></span>
+								{if $msgs->isError()}
+								<h4>Wystąpiły błędy: </h4>
+								<ol>
+									{foreach $msgs->getErrors() as $err}
+										{strip}
+									<li>{$err}</li>
+									{/strip}
+									{/foreach}
+								</ol>
+								{/if}
+
+								{if isset($res->result)}
+									<h4>Kwota całkowita kredytu:</h4>
+									<p>{$res->kwota_calkowita} zł</p>
+									<h4>Miesięczna rata:</h4>
+									<p>{$res->result} zł</p>
+								{/if}
+							</section>
+						</div>
+					</section>
+				</div>
+			</section>
+
+			<!-- Footer -->
+				<footer id="footer">
+					<ul class="copyright">
+						<li>&copy; Untitled. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+					</ul>
+				</footer>
+		</div>
+
+		<!-- Scripts -->
+			<script src="{$conf->app_url}/assets/js/jquery.min.js"></script>
+			<script src="{$conf->app_url}/assets/js/jquery.dropotron.min.js"></script>
+			<script src="{$conf->app_url}/assets/js/jquery.scrollex.min.js"></script>
+			<script src="{$conf->app_url}/assets/js/browser.min.js"></script>
+			<script src="{$conf->app_url}/assets/js/breakpoints.min.js"></script>
+			<script src="{$conf->app_url}/assets/js/util.js"></script>
+			<script src="{$conf->app_url}/assets/js/main.js"></script>
+
+	</body>
+</html>
